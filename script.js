69de139b4fdd0d98254c34ae2a101baf6eac9d3b
macccc383 <helloimcoolerthanu@gmail.com>
2025-08-21 17:07:32 +0700

    Implement carousel homepage

--- script.js
+++ script.js
@@ -127,8 +127,39 @@ function initAnimations() {
   document.querySelectorAll('.animate').forEach(el => observer.observe(el));
 }
 
+function initCarousel() {
+  const images = Array.from(document.querySelectorAll('.image-item img'));
+  if (!images.length) return;
+  const imageSets = [
+    ['NEWIMAGES/378eab1265ecedb2b4fd10.jpg', 'NEWIMAGES/46776ce8a2162a48730711.jpg', 'NEWIMAGES/51ee9f72518cd9d2809d5.jpg'],
+    ['NEWIMAGES/572ccab0044e8c10d55f4.jpg', 'NEWIMAGES/6d11358dfb73732d2a627.jpg', 'NEWIMAGES/7402229eec60643e3d711.jpg'],
+    ['NEWIMAGES/76da7245bcbb34e56daa12.jpg', 'NEWIMAGES/c83f18a3d65d5e03074c2.jpg', 'NEWIMAGES/c976cce902178a49d3068.jpg'],
+    ['NEWIMAGES/c9791fe5d11b5945000a9.jpg', 'NEWIMAGES/ce2859b4974a1f14465b6.jpg', 'NEWIMAGES/f03bc0a40e5a8604df4b13.jpg']
+  ];
+
+  let index = 0;
+
+  function showSet(i) {
+    images.forEach((img, idx) => {
+      img.src = imageSets[i][idx];
+    });
+  }
+
+  showSet(index);
+
+  setInterval(() => {
+    images.forEach(img => img.classList.add('fade'));
+    setTimeout(() => {
+      index = (index + 1) % imageSets.length;
+      showSet(index);
+      images.forEach(img => img.classList.remove('fade'));
+    }, 1000);
+  }, 5000);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   initLanguage();
   initTheme();
   initAnimations();
+  initCarousel();
 });
