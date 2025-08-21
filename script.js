05748569cda7b61023f21760256789e76138508a
macccc383 <helloimcoolerthanu@gmail.com>
2025-08-21 17:31:12 +0700

    Refine hero overlay and dynamic colors

--- script.js
+++ script.js
@@ -143,6 +143,13 @@ function initCarousel() {
     images.forEach((img, idx) => {
       img.src = imageSets[i][idx];
     });
+    if (images[1]) {
+      if (images[1].complete) {
+        updateOverlayColor();
+      } else {
+        images[1].addEventListener('load', updateOverlayColor, { once: true });
+      }
+    }
   }
 
   showSet(index);
@@ -155,6 +162,28 @@ function initCarousel() {
       images.forEach(img => img.classList.remove('fade'));
     }, 1000);
   }, 5000);
+}
+
+function updateOverlayColor() {
+  const img = document.querySelector('.center-item img');
+  if (!img) return;
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  let r = 0, g = 0, b = 0;
+  const total = data.length / 4;
+  for (let i = 0; i < data.length; i += 4) {
+    r += data[i];
+    g += data[i + 1];
+    b += data[i + 2];
+  }
+  r /= total; g /= total; b /= total;
+  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  const color = luminance > 128 ? '#000000' : '#ffffff';
+  document.documentElement.style.setProperty('--overlay-color', color);
 }
 
 function initNavScroll() {
